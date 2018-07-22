6aa1b1ea565f20926b2600143d6f22d743ffe436
WroMaciej <maciej.a.wroblewski@capgemini.com>
2018-07-22 10:26:20 +0200

    Starting with Spring 2.0.3 default template

---
+++ src/main/java/com/capgemini/jstk/boardbuddy/BoardBuddyApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.jstk.boardbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardBuddyApplication.class, args);
+	}
+}
