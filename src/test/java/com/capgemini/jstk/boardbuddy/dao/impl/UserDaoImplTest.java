c00d1b8986e4d5c2ae109bddd37eee12a8cbf88b
WroMaciej <maciej.a.wroblewski@capgemini.com>
2018-07-24 11:27:39 +0200

    First tests

--- src/test/java/com/capgemini/jstk/boardbuddy/dao/impl/UserDaoImplTest.java
+++ src/test/java/com/capgemini/jstk/boardbuddy/dao/impl/UserDaoImplTest.java
@@ -1,11 +1,16 @@
 package com.capgemini.jstk.boardbuddy.dao.impl;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+//import static org.hamcrest.CoreMatchers.is;
+
 
 import com.capgemini.jstk.boardbuddy.dao.UserDao;
 import com.capgemini.jstk.boardbuddy.dao.impl.mock.CommonDatabaseMock;
@@ -37,7 +42,6 @@ public class UserDaoImplTest {
 		// when
 		UserDto userDto = userDao.findById(1).get();
 		// then
-		//assertThat(userDto.getFirstName(), is());
-		
+		assertEquals("John", userDto.getFirstName());	
 	}
 }
---
+++ src/test/java/com/capgemini/jstk/boardbuddy/service/UserServiceTest.java
@@ -0,0 +1,5 @@
+package com.capgemini.jstk.boardbuddy.service;
+
+public class UserServiceTest {
+
+}
